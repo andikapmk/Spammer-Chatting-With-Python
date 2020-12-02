458e3add5f5ae2e9bda008c0551db64e8959f0f4
mankfunky <andika.p.m.k217@gmail.com>
2020-12-02 21:09:10 +0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,10 +1,13 @@
 import time
 import pyautogui
-
-pesan = "sayang"
-jumlah = 50
+print("SPAMMER CHATTING BY ANDIKAPMK")
+print("")
+pesan = input("masukan pesan yang akan di kirim : ")
+jumlah = int(input("masukan jumlah pesan : "))
 
 time.sleep(2)
 
 for i in range(jumlah):
     pyautogui.typewrite(pesan + "\n")
+print("")
+print("spam selesai")
